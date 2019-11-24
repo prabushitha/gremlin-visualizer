dd8ab4ea83130903f0849c5e975a11622c2e1fc9
umeshjay <umesh.jayasinghe@syscolabs.com>
2019-11-25 01:22:13 +0530

    removed graph configurer

--- src/reducers/optionReducer.js
+++ src/reducers/optionReducer.js
@@ -23,12 +23,6 @@ const initialState = {
         updateInterval: 25
       }
     },
-    configure: function(option, path) {
-      if (path.indexOf("smooth") !== -1 || option === "smooth") {
-        return true;
-      }
-      return false;
-    },
     nodes: {
       shape: "dot",
       size: 20,
